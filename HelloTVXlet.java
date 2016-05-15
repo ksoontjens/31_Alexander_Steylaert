eecfc3210418df6c08416c9278c57f60c954d0c6
AlexanderSteyaert <alexanderateyaert@gmail.com>
2016-05-15 18:47:40 +0200

    Base

---
+++ HelloTVXlet.java
@@ -0,0 +1,28 @@
+package hellotvxlet;
+
+import javax.tv.xlet.*;
+
+public class HelloTVXlet implements Xlet {
+    
+  
+    public HelloTVXlet() {
+        
+    }
+
+    public void initXlet(XletContext context) throws XletStateChangeException {
+        
+    }
+
+    public void startXlet() {
+        
+    }
+
+    public void pauseXlet() {
+        
+    }
+
+    public void destroyXlet(boolean unconditional) {
+     
+    }      
+      
+}

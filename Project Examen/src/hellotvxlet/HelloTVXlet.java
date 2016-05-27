9ae2ab535175e036f3adfac3edeba9902e806cf9
AlexanderSteyaert <alexanderateyaert@gmail.com>
2016-05-27 21:07:14 +0200

    v1.0

--- Project Examen/src/hellotvxlet/HelloTVXlet.java
+++ Project Examen/src/hellotvxlet/HelloTVXlet.java
@@ -3,41 +3,69 @@ package hellotvxlet;
 import javax.tv.xlet.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
+import org.davic.resources.ResourceProxy;
 import org.havi.ui.*;
 import org.havi.ui.event.HActionListener;
 import javax.tv.xlet.XletContext;
 import javax.tv.xlet.XletStateChangeException;
+import org.davic.resources.ResourceClient;
+import org.havi.ui.event.HBackgroundImageEvent;
+import org.havi.ui.event.HBackgroundImageListener;
 
-public class HelloTVXlet implements Xlet, HActionListener{
-    HScene scene;
+public class HelloTVXlet implements Xlet, HActionListener,
+                                    ResourceClient, HBackgroundImageListener {
+    private HScene scene;
+    private HScreen screen;
+    private HBackgroundDevice bgDev;
+    private HStillImageBackgroundConfiguration bgConfig;
+    private HBackgroundImage bgImg;
     InterfaceDrawer imager;
-    
+    HTextLayoutManager textManager;
     private XletContext actueleXletContext;
-    private boolean debug=true;
+    static boolean debug=true;
     String vote;
     
-    Color geel = new Color(255,255,51,200);
+    //custom colors
+    static Color purple =       new Color(100,113,194);
+    static Color infoColor =    new Color(29,108,142,200);
+    static Color green =        new Color(89,201,134);
+    static Color blue =         new Color(86,154,184,255);
     
+    //graphic elements
     HTextButton option1, option2, option3, option4;
-    HTextButton blanco, confirm, goback;
-    HStaticText title, confirmTitle;
+    HTextButton blanco, confirm, goback, help;
+    HStaticText title, confirmTitle, helptxt;
 
     public void initXlet(XletContext context) throws XletStateChangeException {
         
       if(debug) System.out.println("Xlet Initialize");
       this.actueleXletContext = context;
       
-      scene=HSceneFactory.getInstance().getDefaultHScene();
-     
+      scene = HSceneFactory.getInstance().getDefaultHScene();
+      screen = HScreen.getDefaultHScreen();
+      bgDev = screen.getDefaultHBackgroundDevice();
+      bgDev.reserveDevice(this);
+      HBackgroundConfigTemplate bgConfigTemplate = new HBackgroundConfigTemplate();
+      bgConfigTemplate.setPreference(HBackgroundConfigTemplate.STILL_IMAGE, 
+                                     HBackgroundConfigTemplate.REQUIRED);
+      try {
+          bgConfig = (HStillImageBackgroundConfiguration) bgDev.getBestConfiguration(bgConfigTemplate);
+          bgDev.setBackgroundConfiguration(bgConfig);
+      } catch (Exception ex) {
+          if(HelloTVXlet.debug){
+            System.out.println("Error: background ("+ex.toString()+")");
+          
+      }}
+      
       imager = null;
-      imager = new InterfaceDrawer();
+      imager = new InterfaceDrawer("hoi");
       
       
       //template aanmaken
-     HSceneTemplate sceneTemplate = new HSceneTemplate();
+     /*HSceneTemplate sceneTemplate = new HSceneTemplate();
      
      sceneTemplate.setPreference(HSceneTemplate.SCENE_SCREEN_DIMENSION, 
-             new HScreenDimension(1.0f, 1.0f), HSceneTemplate.REQUIRED);
+             new HScreenDimension(1.0f, 1.0f), HSceneTemplate.REQUIRED);*/
      
                                      // ( X, Y, W, H )
         //title
@@ -48,65 +76,80 @@ public class HelloTVXlet implements Xlet, HActionListener{
        // img1 = this.getToolkit().getImage("alien.jpg");
         
         //buttons + images
-        option1=new HTextButton("Batman",10,400,160,40);
+        option1=new HTextButton("Batman",        10, 360, 160, 40);
         
-        option2=new HTextButton("A. Lien",190,400,160,40);
+        option2=new HTextButton("A. Lien",      190, 360, 160, 40);
         
-        option3=new HTextButton("J. Bravo",370,400,160,40);
+        option3=new HTextButton("J. Bravo",     370, 360, 160, 40);
         
-        option4=new HTextButton("S. Verhulst",550,400,160,40);
+        option4=new HTextButton("S. Verhulst",  550, 360, 160, 40);
         
-        blanco=new HTextButton("Blanco", 300, 500, 120, 50);
+        blanco=new HTextButton("blanco",        300, 500, 120, 50);
         
-        confirm=new HTextButton("Confirm", 180, 500, 120, 50);
+        confirm=new HTextButton("confirm",      180, 500, 120, 50);
         
-        goback=new HTextButton("Cancel", 300, 500, 120, 50);
+        goback=new HTextButton("cancel",        300, 500, 120, 50);
+        
+        help=new HTextButton("?",               10,   10,  30, 30);
         
         //set properties for buttons
-        HTextButton[] button = {option1,option2,option3,option4,blanco,confirm,goback};
+        HTextButton[] button = {option1,option2,option3,option4,blanco,confirm,goback,help};
         for (int i = 0; i < button.length; i++) {
-         button[i].setBackground(geel);
+         button[i].setBackground(purple);
          button[i].setBackgroundMode(HVisible.BACKGROUND_FILL);
          button[i].setBordersEnabled(false);
          button[i].setForeground(Color.BLACK);
          button[i].setActionCommand(button[i].getTextContent(HState.DISABLED_STATE));
          button[i].addHActionListener(this);
       }
-        confirm.setBackground(Color.GREEN);
-        goback.setBackground(Color.RED);
+        confirm.setBackground(green);
+        help.setBackground(infoColor);
+        help.setForeground(Color.WHITE);
         
         //Focustraversals
-        option1.setFocusTraversal(null, blanco, option4, option2);
-        option2.setFocusTraversal(null, blanco, option1, option3);
-        option3.setFocusTraversal(null, blanco, option2, option4);
-        option4.setFocusTraversal(null, blanco, option3, option1);
+        option1.setFocusTraversal(help, blanco, option4, option2);
+        option2.setFocusTraversal(help, blanco, option1, option3);
+        option3.setFocusTraversal(help, blanco, option2, option4);
+        option4.setFocusTraversal(help, blanco, option3, option1);
+        help.setFocusTraversal(blanco, option1, option1, option4);
         
-        blanco.setFocusTraversal(option1, null, null, null);
+        blanco.setFocusTraversal(option1, help, null, null);
         confirm.setFocusTraversal(null, null, null, goback);
         goback.setFocusTraversal(null, null, confirm, null);
-    }
-
-    public void startXlet() {
         
-        scene.add(imager);
+        
+        
+    }
+    
+    public void showNominees(){
         scene.add(title);   scene.add(option1);
         scene.add(option2); scene.add(option3);
         scene.add(option4); scene.add(blanco);
+        scene.add(help);
+        
+        
+        scene.add(imager);
+        option1.requestFocus();
+        
+    }
+
+    public void startXlet() {
+        showNominees();
+        
+        bgImg = new HBackgroundImage("project/background.m2v");
+        bgImg.load(this);
         
         
         scene.validate();
         scene.setVisible(true);
-        
-        option1.requestFocus();
     }
     
     public void actionPerformed(ActionEvent arg0) {
         
-        System.out.println(arg0.getActionCommand());
-        
         if(arg0.getActionCommand() == confirm.getTextContent(HState.DISABLED_STATE))
         {
-            System.out.println("Voted for: "+vote);
+            //Vote comfirmed
+            System.out.println("Succes: User voted for: "+vote);
             scene.removeAll();
             confirmTitle.setTextContent("Thank you for voting!", HState.NORMAL_STATE);
             scene.add(confirmTitle);
@@ -115,22 +158,54 @@ public class HelloTVXlet implements Xlet, HActionListener{
             
             //whiteHouse.sendVote(vote);
         }
-        else if (arg0.getActionCommand() == goback.getTextContent(HState.DISABLED_STATE)){
+        else if (arg0.getActionCommand() == "cancel"){
+            //Start over
+            System.out.println("Succes: User changed mind for: "+vote);
+            
             scene.removeAll();
+            showNominees();
             scene.repaint();
-            startXlet();
+        }
+        else if (arg0.getActionCommand() == help.getTextContent(HState.DISABLED_STATE)){
+            
+            scene.removeAll();
+
+            System.out.println("Succes: User doesn't understand application");
+            
+            confirmTitle=new HStaticText("Information" , 0, 30, 720, 50);
+            helptxt=new HStaticText(
+                    "1. selecteer favoriete kandidaat en druk op ENTER\n" +
+                    "2. Druk op ENTER ter confirmatie\n" +
+                    "3. Wanneer u het dankwoord te zien krijgt, is uw stem opgeslagen\n" +
+                    "\n" +
+                    "Contacteer ons bij problemen op: info@votingapplicatieprojectje.be"
+                    , 10, 50, 705, 500);
+            
+            goback.setTextContent("Go back", HState.DISABLED_STATE);
+            scene.add(confirmTitle);
+            scene.add(helptxt);
+            scene.add(goback);
+
+            scene.validate();
+            scene.repaint();
+
+            goback.requestFocus();
         }
         else {
-            vote=arg0.getActionCommand();
+            //Voting comfirmation
+            if(arg0.getActionCommand() != blanco.getTextContent(HState.DISABLED_STATE)){
+                vote="for "+arg0.getActionCommand();
+            }
+            else {vote = arg0.getActionCommand();}
             
-            confirmTitle=new HStaticText("You chose "+arg0.getActionCommand()+", are you sure?" , 0, 30, 720, 50);
+            confirmTitle=new HStaticText("You are voting "+vote+", are you sure?" , 0, 30, 720, 50);
             confirmTitle.setBackground(Color.LIGHT_GRAY);
             confirmTitle.setBackgroundMode(HVisible.NO_BACKGROUND_FILL);
-
+            goback.setTextContent("Cancel", HState.DISABLED_STATE);
 
             scene.removeAll();
 
-            System.out.println("hoi");
+            System.out.println("Succes: User is voting for "+arg0.getActionCommand()+"...");
 
             scene.add(confirmTitle);
             scene.add(confirm); scene.add(goback);
@@ -149,6 +224,37 @@ public class HelloTVXlet implements Xlet, HActionListener{
 
     public void destroyXlet(boolean unconditional) throws XletStateChangeException {
         throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public boolean requestRelease(ResourceProxy proxy, Object requestData) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void release(ResourceProxy proxy) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void notifyRelease(ResourceProxy proxy) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void imageLoaded(HBackgroundImageEvent e) {
+        if(HelloTVXlet.debug){
+            System.out.println("Succes: backgroundimage loaded...");
+        }
+        try {
+            bgConfig.displayImage(bgImg);
+        } catch (Exception ex) {
+            if(HelloTVXlet.debug){
+            System.out.println("Error: backgroundimage "+ex.toString()+")");
+        }
+        }
+    }
+
+    public void imageLoadFailed(HBackgroundImageEvent e) {
+        if(HelloTVXlet.debug){
+            System.out.println("Error: backgroundimage not loaded");
+        }
     }
       
 }
--- Project Examen/src/hellotvxlet/InterfaceDrawer.java
+++ Project Examen/src/hellotvxlet/InterfaceDrawer.java
@@ -12,12 +12,16 @@ import org.havi.ui.*;
  * @author student
  */
 public class InterfaceDrawer extends HComponent {
-    Image img1, img2, img3, img4;
+    private Image img1, img2, img3, img4;
+    private MediaTracker mt;
+    Color back = HelloTVXlet.blue;
     
     
-    public InterfaceDrawer()
+    
+    public InterfaceDrawer(String test)
     {
-        this.setBounds(0,0,720,56);
+        System.out.println("out: "+test);
+        
         img1 = this.getToolkit().getImage("project/batman.png");
         img2 = this.getToolkit().getImage("project/alien.png");
         img3 = this.getToolkit().getImage("project/johnney bravo.png");
@@ -25,31 +29,49 @@ public class InterfaceDrawer extends HComponent {
         
         //images: C:\Program Files\TechnoTrend\TT-MHP-Browser\fileio\DSMCC\0.0.3
         
-        MediaTracker mt = new MediaTracker(this);
-        
+        mt = new MediaTracker(this);
         mt.addImage(img1, 0);
         mt.addImage(img2, 1);
         mt.addImage(img3, 2);
         mt.addImage(img4, 3);
         
-        
-        //this.repaint();
-        
         try {
-            mt.waitForAll();
-        } catch (InterruptedException ex) {
-            ex.printStackTrace();
-            
+        mt.waitForAll();
+        if(HelloTVXlet.debug){
+            System.out.println("Succes: MT");
         }
+        } catch (Exception ex) {
+        if(HelloTVXlet.debug){
+            System.out.println("Error: mediatracker ("+ex.toString()+")");
+        }
+        }
+        
+        
+        this.setBounds(0,0,720,576);
+        
+        
         
     }
     
     public void paint(Graphics g)
     {
-        g.drawImage(img1, 10, 160, null);
-        g.drawImage(img2, 190, 160, null);
-        g.drawImage(img3, 370, 160, null);
-        g.drawImage(img4, 550, 160, null);
+        g.setColor(back);
+        
+        g.fillRoundRect (5, 200, 170, 205, 5, 5);
+        g.drawImage(img1, 10,  200, null);
+        
+        g.fillRoundRect (185, 200, 170, 205, 5, 5);
+        g.drawImage(img2, 190, 200, null);
+        
+        g.fillRoundRect (365, 200, 170, 205, 5, 5);
+        g.drawImage(img3, 370, 200, null);
+        
+        g.fillRoundRect (545, 200, 170, 205, 5, 5);
+        g.drawImage(img4, 550, 200, null);
+        
+        if(HelloTVXlet.debug){
+            System.out.println("Succes: paint");
+        }
     }
     
     
